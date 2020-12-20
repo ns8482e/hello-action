b530b3c9b8ee83af9b78b066a655a9e423f18668
Niraj Soni <23270244+ns8482e@users.noreply.github.com>
2020-12-19 19:20:23 -0600

    ci: update action

--- actions/ccbypr/index.js
+++ actions/ccbypr/index.js
@@ -31,15 +31,22 @@ try {
   }catch(err){
     error = err;
   }
-  
+
+  console.log(result.status);
+
   console.log(result.message);
 
   let state = 'success';
   if(result && result.status === "success"){
+
+    console.log('in success');
+
     core.setOutput('success',true);
+
     state = 'success';
   }
   else{
+    console.log('in failure');
     core.setOutput('success',false);
     state = 'failure';
   }
@@ -56,6 +63,8 @@ try {
       context: contextName,
     },
   );
+
+  console.log('error is' + error);
 
   if (error) {
     throw error;
